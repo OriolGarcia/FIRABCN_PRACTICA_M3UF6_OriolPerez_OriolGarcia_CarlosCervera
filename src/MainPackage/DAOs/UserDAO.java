3ed14192a78d0c5f0be64f543f004db30ce67955
OriolGarcia <130761.clot@fje.edu>
2017-03-30 15:42:00 +0200

    dao user

--- src/MainPackage/DAOs/UserDAO.java
+++ src/MainPackage/DAOs/UserDAO.java
@@ -3,9 +3,10 @@ package MainPackage.DAOs;
 /**
  * Created by ogs10_000 on 29/03/2017.
  */
-public class UserDAO {
+public interface UserDAO {
 
-
-
-
+        public boolean AddUser();
+    public boolean UpdateUser();
+    public boolean DeleteUser();
+    public boolean LoginUser(String User, String password);
 }
