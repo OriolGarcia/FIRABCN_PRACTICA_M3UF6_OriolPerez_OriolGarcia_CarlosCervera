577df497f7bf3b1864cb7459f4fa10ec528f1e88
OriolGarcia <130761.clot@fje.edu>
2017-04-19 20:24:30 +0200

    JPA init

--- src/MainPackage/JPA/EconomiaFira.java
+++ src/MainPackage/JPA/EconomiaFira.java
@@ -4,7 +4,6 @@ import org.eclipse.persistence.jpa.jpql.parser.DateTime;
 
 import javax.persistence.*;
 import java.util.GregorianCalendar;
-
 /**
  * Created by ogs10_000 on 19/04/2017.
  */
@@ -14,11 +13,19 @@ public class EconomiaFira {
     @Id
     private int FiraID;
     @Id
-    private GregorianCalendar Date;
+    private GregorianCalendar date;
     private int numVisitants;
-    private float Recaptació;
+    private float recaptació;
     @ManyToOne
     @JoinColumn(name = "FiraID")
     private Fires fires;
+        EconomiaFira(int FiraID,GregorianCalendar date, int numVisitants,float recaptació){
+            this.FiraID=FiraID;
+            this.date=date;
+            this.numVisitants=numVisitants;
+            this.recaptació=recaptació;
 
+
+
+            }
 }
