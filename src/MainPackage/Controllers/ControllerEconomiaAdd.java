7cf81af27f3754cf713f1b41d5df04217473a85c
oriol <orpenu1516daw2@gmail.com>
2017-04-26 19:48:56 +0200

    DatePicker a Add Economia

--- src/MainPackage/Controllers/ControllerEconomiaAdd.java
+++ src/MainPackage/Controllers/ControllerEconomiaAdd.java
@@ -7,12 +7,16 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.Date;
 
 import java.sql.Connection;
@@ -26,6 +30,9 @@ public class ControllerEconomiaAdd {
 
     @FXML
     private TextField txtNumVisitants;
+
+    @FXML
+    private DatePicker dtDataActual;
 
     @FXML
     private ComboBox<Item> ComBFires;
@@ -49,11 +56,16 @@ public class ControllerEconomiaAdd {
         float EntPerVis;
 
         try {
-            DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-            Date date = new Date();
-            String curr = dateFormat.format(date);
-            System.out.println(curr);
-            Date currDate = dateFormat.parse(curr);
+            //DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+            //Date date = new Date();
+            //Date date =
+            //String curr = dateFormat.format(date);
+
+            LocalDate localDate1 = dtDataActual.getValue();
+            Instant instant = Instant.from(localDate1.atStartOfDay(ZoneId.systemDefault()));
+            Date currDate = Date.from(instant);
+            //System.out.println(curr);
+            //Date currDate = dateFormat.parse(curr);
             int numVisitants = Integer.parseInt(txtNumVisitants.getText());
             EconomiaDAOImplement ecoDAOImpl = new EconomiaDAOImplement();
 
